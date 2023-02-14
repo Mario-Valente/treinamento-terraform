39897f4b30d6937d8c60f155a0420e06bda2cbb9
Mario Valente <mario.valente@atlastechnol.com>
2023-02-14 10:14:39 -0300

    feat: :zap: Add new network

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "aws_iam_user"{
     type = string
     default = "teste_iam"
 }
+variable "cidr" {
+  
+  default = "10.0.0.0/24"
+}
+
+variable "name_vpc" {
+    type = string
+    default = "my_vpc"
+  
+}
